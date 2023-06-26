7e3cb1661e373b6ea6f7bd2f082860d24b5dba9a
fire1fly <danyseidev@gmail.com>
2023-06-26 23:02:16 +0800

    feat: add redux, store provider, reducer manager

---
+++ src/app/providers/StoreProvider/config/store.ts
@@ -0,0 +1,44 @@
+import {
+  CombinedState, Reducer, ReducersMapObject, configureStore,
+} from '@reduxjs/toolkit';
+import { counterReducer } from 'entities/Counter';
+import { $api } from 'shared/api/api';
+import { NavigateOptions, To } from 'react-router-dom';
+import { StateSchema, ThunkExtraArg } from './StateSchema';
+import { createReducerManager } from './reducerManager';
+
+export function createReduxStore(
+  initialState?: StateSchema,
+  asyncReducers?: ReducersMapObject<StateSchema>,
+  navigate?: (to: To, options?: NavigateOptions) => void,
+) {
+  const rootReducers: ReducersMapObject<StateSchema> = {
+    counter: counterReducer,
+    ...asyncReducers,
+  };
+
+  const reducerManager = createReducerManager(rootReducers);
+
+  const extraArg: ThunkExtraArg = {
+    api: $api,
+    navigate,
+  };
+
+  const store = configureStore({
+    reducer: reducerManager.reduce as Reducer<CombinedState<StateSchema>>,
+    devTools: __IS_DEV__,
+    preloadedState: initialState,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+      thunk: {
+        extraArgument: extraArg,
+      },
+    }),
+  });
+
+  // @ts-ignore - temporary before fix types
+  store.reducerManager = reducerManager;
+
+  return store;
+}
+
+export type AppDispatch = ReturnType<typeof createReduxStore>['dispatch'];
