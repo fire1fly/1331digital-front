19ce5ffcb712e0153fc16ee2ced767af613b94f9
fire1fly <danyseidev@gmail.com>
2023-07-06 22:06:27 +0800

    feat: add ProjectCard

---
+++ src/entities/ProjectCard/model/types.ts
@@ -0,0 +1,6 @@
+export interface IProjectCard {
+  id: number;
+  preview?: string;
+  logo?: string;
+  tags?: string[];
+}
---
+++ src/entities/ProjectCard/ui/ProjectCard.tsx
@@ -0,0 +1,103 @@
+import { FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
+
+import designImage from 'shared/assets/images/techtag-design.png';
+import frontendImage from 'shared/assets/images/techtag-html.png';
+import backendImage from 'shared/assets/images/techtag-php.png';
+import { IProjectCard } from '../model/types';
+import cls from './ProjectCard.module.scss';
+
+const tagsConf: Record<string, string> = {
+  design: designImage,
+  frontend: frontendImage,
+  backend: backendImage,
+};
+
+const classesMods: Record<string, string> = {
+  _c_2: cls._c_2,
+  _c_3: cls._c_3,
+  _design: cls._design,
+  _frontend: cls._frontend,
+  _backend: cls._backend,
+};
+
+interface ProjectCardProps {
+  className?: string;
+  item: IProjectCard
+}
+
+export const ProjectCard: FC<ProjectCardProps> = (props) => {
+  const {
+    className,
+    item,
+  } = props;
+
+  const {
+    preview,
+    logo,
+    tags,
+  } = item;
+
+  return (
+    <div className={classNames(cls.ProjectCard, className)}>
+      <div className={classNames(cls.ProjectCard_backframe, classesMods[`_c_${tags?.length}`])}>
+        {
+          tags?.map((tag) => (
+            <div
+              key={tag}
+              className={classesMods[`_${tag}`]}
+            />
+          ))
+        }
+      </div>
+      <div className={classNames(cls.ProjectCard_frame)}>
+        <img
+          src={preview}
+          className={classNames(cls.ProjectCard_preview)}
+          alt="project preview"
+        />
+        <div className={classNames(cls.ProjectCard_backdrop)} />
+
+        <div className={classNames(cls.ProjectCard_inner)}>
+          <div className={classNames(cls.ProjectCard_techtags, 'techtags')}>
+            {
+              tags?.map((tag) => (
+                <div
+                  key={tag}
+                  className={classNames(cls.ProjectCard_techtags_tag, 'techtags-tag')}
+                >
+                  <img src={tagsConf[tag]} alt="tag" />
+                </div>
+              ))
+            }
+          </div>
+
+          <div className={classNames(cls.ProjectCard_logo)}>
+            <img
+              src={logo}
+              alt="project logo"
+            />
+          </div>
+
+          <div className={classNames(cls.ProjectCard_bottom)}>
+            <div className={classNames(cls.ProjectCard_info)}>
+              <div className={classNames(cls.ProjectCard_type, '_text-upper', '_c-yellow-1')}>Вёрстка макета</div>
+              <div className={classNames(cls.ProjectCard_sub)}>86 страниц</div>
+            </div>
+
+            {/* TODO: pagination */}
+          </div>
+
+          <Button
+            theme={ButtonTheme.YELLOW}
+            size={ButtonSize.M}
+            className={classNames(cls.ProjectCard_btn, '_text-upper')}
+          >
+            Посмотреть
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
