e8ed1e3c373301b5492c7b117044b33c30fdd90e
fire1fly <danyseidev@gmail.com>
2023-11-23 09:05:00 +0800

    config: add contacts type and init definition

---
+++ src/widgets/MainHeader/model/contacts.ts
@@ -0,0 +1,24 @@
+import iconTelegram from 'shared/assets/icons/icon-telegram-red.svg';
+import iconMail from 'shared/assets/icons/icon-message-red.svg';
+
+export interface Contact {
+  to: string;
+  icon?: React.VFC<React.SVGProps<SVGSVGElement>>;
+  type?: string;
+  label?: string;
+}
+
+export const contactList: Contact[] = [
+  {
+    to: 'https://t.me/test',
+    icon: iconTelegram,
+    type: 'telegram',
+    label: '@1331support',
+  },
+  {
+    to: 'mailto:support@1331.digital',
+    icon: iconMail,
+    type: 'e-mail',
+    label: '@1331support',
+  },
+];
