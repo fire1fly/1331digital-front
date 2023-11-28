2249ddb627db1dd5065a4f799b301e108d1cf940
fire1fly <danyseidev@gmail.com>
2023-11-28 15:36:00 +0800

    feat: add MainPage, add lazy wrapper

---
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -0,0 +1,65 @@
+import { ProjectCard } from 'entities/ProjectCard';
+import preview1 from 'shared/assets/images/main-project-card-preview-1.png';
+import preview2 from 'shared/assets/images/main-project-card-preview-2.png';
+import logo1 from 'shared/assets/images/project-logo-example-1.png';
+import logo2 from 'shared/assets/images/project-logo-example-2.png';
+import logo3 from 'shared/assets/images/project-logo-example-3.png';
+import { ProjectCardSlider } from 'features/ProjectCardSlider';
+import cls from './MainPage.module.scss';
+
+const projects = [
+  {
+    id: 1,
+    preview: preview1,
+    logo: logo1,
+    tags: ['design', 'frontend', 'backend'],
+  },
+  {
+    id: 2,
+    preview: preview2,
+    logo: logo2,
+    tags: ['design', 'frontend'],
+  },
+  {
+    id: 3,
+    preview: preview1,
+    logo: logo3,
+    tags: ['design'],
+  },
+];
+
+const MainPage = () => (
+  <div className={cls.MainPage}>
+    <div className={cls.MainPage_row_top}>
+      <ProjectCard
+        item={projects[0]}
+      />
+      <ProjectCard
+        item={projects[1]}
+      />
+      <ProjectCard
+        item={projects[2]}
+      />
+    </div>
+    <div className={cls.MainPage_row_bottom}>
+      <ProjectCardSlider
+        items={projects}
+        className={cls.MainPage_pcard}
+      />
+      <ProjectCardSlider
+        items={projects}
+        className={cls.MainPage_pcard}
+      />
+      <ProjectCardSlider
+        items={projects}
+        className={cls.MainPage_pcard}
+      />
+      <ProjectCardSlider
+        items={projects}
+        className={cls.MainPage_pcard}
+      />
+    </div>
+  </div>
+);
+
+export default MainPage;
