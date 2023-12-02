1f359c85d5a23a5aaf1a6a8ff67f680e575a4bb8
fire1fly <danyseidev@gmail.com>
2023-12-02 14:33:00 +0800

    config: add routes types configuration

--- src/app/providers/router/config/routeConfig.tsx
+++ src/app/providers/router/config/routeConfig.tsx
@@ -1,21 +1,35 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
-
-type AppRoutesProps = RouteProps & {
-  authOnly?: boolean;
-}
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
   MAIN = 'main',
+  TEAM = 'team',
+  CONTACTS = 'contacts',
+  SERVICES = 'services',
+  NOT_FOUND = 'notFound'
+}
+
+type AppRoutesProps = RouteProps & {
+  authOnly?: boolean;
+  nested?: Record<AppRoutes, AppRoutesProps>
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
+  [AppRoutes.TEAM]: 'team',
+  [AppRoutes.CONTACTS]: 'contacts',
+  [AppRoutes.SERVICES]: 'services',
+  [AppRoutes.NOT_FOUND]: '*',
 };
 
-export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
-  [AppRoutes.MAIN]: {
-    path: RoutePath.main,
-    element: <MainPage />,
-  },
-};
+// export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
+//   [AppRoutes.MAIN]: {
+//     path: RoutePath.main,
+//     element: <MainPage />,
+//   },
+//   [AppRoutes.NOT_FOUND]: {
+//     path: RoutePath.notFound,
+//     element: <NotFoundPage />,
+//   },
+// };
