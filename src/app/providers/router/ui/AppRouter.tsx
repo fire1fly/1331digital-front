d8e92e574998a2f6691d1abedef2cad0a8e70c0f
fire1fly <danyseidev@gmail.com>
2023-12-03 12:01:00 +0800

    chore: change routes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,35 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 
 import { MainLayout } from 'app/layouts/MainLayout/MainLayout';
 import { NotFoundPage } from 'pages/NotFoundPage';
+import { MainPage } from 'pages/MainPage';
 
-export const AppRouter = memo(() => {
-  console.log('temp');
-  // const routes = useMemo(() => Object.values(routeConfig).filter((route) => !(route.authOnly && !isAuth)), [isAuth]);
-
-  return (
-    <Routes>
-      {/* {
-        Object.values(routeConfig).map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={(
-              <Suspense fallback={<PageLoader />}>
-                <div className="page-wrapper">
-                  {route.element}
-                </div>
-              </Suspense>
-            )}
-          />
-        ))
-      } */}
-      <Route path="/" element={<MainLayout />}>
-        <Route path="portfolio" element={<p>Портфолио</p>} />
-        <Route path="team" element={<p>Команда</p>} />
-        <Route path="contacts" element={<p>Контакты</p>} />
-        <Route path="services" element={<p>Услуги</p>} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  );
-});
+export const AppRouter = memo(() => (
+  <Routes>
+    <Route path="/" element={<MainLayout />}>
+      <Route path="" element={<MainPage />} />
+      <Route path="team" element={<p>Команда</p>} />
+      <Route path="contacts" element={<p>Контакты</p>} />
+      <Route path="services" element={<p>Услуги</p>} />
+    </Route>
+    <Route path="*" element={<NotFoundPage />} />
+  </Routes>
+));
