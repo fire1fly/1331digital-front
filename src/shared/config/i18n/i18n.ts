9d73952bf3794d8d1800ca28b304149fb79fd822
fire1fly <danyseidev@gmail.com>
2023-12-12 22:46:03 +0800

    config: (temp) set translation in sync mode without backend

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -3,22 +3,13 @@ import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
+import en from '../../../../public/locales/en/translation.json';
+import ru from '../../../../public/locales/ru/translation.json';
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
+  // .use(Backend)
+  // .use(LanguageDetector)
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'ru',
     debug: __IS_DEV__,
@@ -26,9 +17,13 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    resources: {
+      en,
+      ru,
     },
+    // backend: {
+    //   loadPath: '/locales/{{lng}}/{{ns}}.json',
+    // },
   });
 
 export default i18n;
