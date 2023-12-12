04bfd110803841e5eb78a5684ad6144f773e0e62
fire1fly <danyseidev@gmail.com>
2023-12-12 21:57:39 +0800

    config: add fonts to tailwind config, enable downlevelIteration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,18 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'hisqaida-bold': ['Hisqaida2018-Bold', 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif'],
+        'intro-black': ['Intro Black', 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif'],
+        maristraltt: ['MagistralTT', 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif'],
+        'magistral-black': ['Magistral Black', 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif'],
+        'alegreya-bold': ['Alegreya Sans SC Bold', 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif'],
+        'geometria-regular': ['Geometria Regular', 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif'],
+        'geometria-bold': ['Geometria Bold', 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif'],
+        couriernew: ['CourierNew', 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
