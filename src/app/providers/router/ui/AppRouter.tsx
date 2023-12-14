ce206a1b024ea9f0a25125faf0dca25ba6967eeb
fire1fly <danyseidev@gmail.com>
2023-12-14 23:56:10 +0800

    feat: add routes for project page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,18 +1,22 @@
 import { memo } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
 import { MainLayout } from 'app/layouts/MainLayout/MainLayout';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import { MainPage } from 'pages/MainPage';
+import { Text, TextColor } from 'shared/ui/Text/Text';
+import { ProjectLayout } from 'app/layouts/ProjectLayout/ProjectLayout';
 
-export const AppRouter = memo(() => (
+export const AppRouter = () => (
   <Routes>
     <Route path="/" element={<MainLayout />}>
-      <Route path="" element={<MainPage />} />
-      <Route path="team" element={<p>Команда</p>} />
-      <Route path="contacts" element={<p>Контакты</p>} />
-      <Route path="services" element={<p>Услуги</p>} />
+      <Route index element={<MainPage />} />
+    </Route>
+    <Route path="/project/:id" element={<ProjectLayout />}>
+      <Route path="frontend" element={<Text tag="h1" color={TextColor.red_1}>Фронтенд</Text>} />
+      <Route path="design" element={(<Text tag="h1" color={TextColor.red_1}>Дизайн</Text>)} />
+      <Route path="backend" element={<Text tag="h1" color={TextColor.red_1}>Бэкенд</Text>} />
     </Route>
     <Route path="*" element={<NotFoundPage />} />
   </Routes>
-));
+);
