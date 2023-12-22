5d325c2aaef7142696f485cfeeafc31b9667b349
fire1fly <danyseidev@gmail.com>
2023-12-22 13:20:00 +0800

    chore: (temp) add request to external domain

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -5,6 +5,7 @@ import logo1 from 'shared/assets/images/project-logo-example-1.png';
 import logo2 from 'shared/assets/images/project-logo-example-2.png';
 import logo3 from 'shared/assets/images/project-logo-example-3.png';
 import { ProjectCardSlider } from 'features/ProjectCardSlider';
+import { useEffect } from 'react';
 import cls from './MainPage.module.scss';
 
 const projects = [
@@ -28,39 +29,45 @@ const projects = [
   },
 ];
 
-const MainPage = () => (
-  <div className={cls.MainPage}>
-    <h1>{__API__}</h1>
-    <div className={cls.MainPage_row_top}>
-      <ProjectCard
-        item={projects[0]}
-      />
-      <ProjectCard
-        item={projects[1]}
-      />
-      <ProjectCard
-        item={projects[2]}
-      />
+const MainPage = () => {
+  useEffect(() => {
+    const response = fetch(`${__API__}projects`).then((response) => response.json()).then((json) => console.log(json));
+  }, []);
+
+  return (
+    <div className={cls.MainPage}>
+      <h1>{__API__}</h1>
+      <div className={cls.MainPage_row_top}>
+        <ProjectCard
+          item={projects[0]}
+        />
+        <ProjectCard
+          item={projects[1]}
+        />
+        <ProjectCard
+          item={projects[2]}
+        />
+      </div>
+      <div className={cls.MainPage_row_bottom}>
+        <ProjectCardSlider
+          items={projects}
+          className={cls.MainPage_pcard}
+        />
+        <ProjectCardSlider
+          items={projects}
+          className={cls.MainPage_pcard}
+        />
+        <ProjectCardSlider
+          items={projects}
+          className={cls.MainPage_pcard}
+        />
+        <ProjectCardSlider
+          items={projects}
+          className={cls.MainPage_pcard}
+        />
+      </div>
     </div>
-    <div className={cls.MainPage_row_bottom}>
-      <ProjectCardSlider
-        items={projects}
-        className={cls.MainPage_pcard}
-      />
-      <ProjectCardSlider
-        items={projects}
-        className={cls.MainPage_pcard}
-      />
-      <ProjectCardSlider
-        items={projects}
-        className={cls.MainPage_pcard}
-      />
-      <ProjectCardSlider
-        items={projects}
-        className={cls.MainPage_pcard}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default MainPage;
