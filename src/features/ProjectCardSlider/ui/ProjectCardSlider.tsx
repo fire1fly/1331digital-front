ab677bd5034ececa30ed9fd2fce325885a148321
fire1fly <danyseidev@gmail.com>
2023-12-27 10:23:00 +0800

    feat: add mousewheel control, external slider support

--- src/features/ProjectCardSlider/ui/ProjectCardSlider.tsx
+++ src/features/ProjectCardSlider/ui/ProjectCardSlider.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { IProjectCard, ProjectCard } from 'entities/ProjectCard';
-import { Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import Swiper, { Mousewheel, Pagination } from 'swiper';
+
+import { Swiper as SwiperEl, SwiperSlide } from 'swiper/react';
 
 import 'swiper/scss';
 import 'swiper/scss/pagination';
@@ -10,38 +11,45 @@ import './ProjectCardSlider.scss';
 
 interface ProjectCardSliderProps {
   className?: string;
+  externalSwiper?: Swiper;
   items: IProjectCard[]
 }
 
 export const ProjectCardSlider: FC<ProjectCardSliderProps> = (props) => {
   const {
     className,
+    externalSwiper,
     items,
   } = props;
 
   return (
-    <Swiper
-      modules={[Pagination]}
-      direction="vertical"
-      className={classNames(className, 'ProjectCardSlider')}
-      pagination={{
-        dynamicBullets: true,
-        clickable: true,
-      }}
-
+    <div
+      onMouseEnter={() => externalSwiper?.mousewheel.disable()}
+      onMouseLeave={() => externalSwiper?.mousewheel.enable()}
     >
-      {
-        items.map((item) => (
-          <SwiperSlide
-            key={item.id}
-          >
-            <ProjectCard
-              item={item}
-              className="ProjectCardSlider-card"
-            />
-          </SwiperSlide>
-        ))
-      }
-    </Swiper>
+      <SwiperEl
+        modules={[Pagination, Mousewheel]}
+        direction="vertical"
+        mousewheel
+        className={classNames(className, 'ProjectCardSlider')}
+        pagination={{
+          dynamicBullets: true,
+          clickable: true,
+        }}
+      >
+        {
+          items.map((item) => (
+            <SwiperSlide
+              key={item.id}
+            >
+              <ProjectCard
+                item={item}
+                className="ProjectCardSlider-card"
+              />
+            </SwiperSlide>
+          ))
+        }
+      </SwiperEl>
+    </div>
   );
 };
