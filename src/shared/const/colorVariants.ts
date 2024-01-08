c520648da712fb4f44b24da268eee71c779965c6
fire1fly <danyseidev@gmail.com>
2024-01-08 12:05:00 +0800

    chore: add colors class objects

---
+++ src/shared/const/colorVariants.ts
@@ -0,0 +1,17 @@
+export const textColorVariants: Record<string, string> = {
+  design: 'text-design',
+  frontend: 'text-frontend',
+  backend: 'text-backend',
+};
+
+export const backgroundColorVariants: Record<string, string> = {
+  design: 'bg-design',
+  frontend: 'bg-frontend',
+  backend: 'bg-backend',
+};
+
+export const borderColorVariants: Record<string, string> = {
+  design: 'border-design',
+  frontend: 'border-frontend',
+  backend: 'border-backend',
+};
