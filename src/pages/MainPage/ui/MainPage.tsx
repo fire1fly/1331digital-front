177cd882b54bd337f44808853a633db3ca8f935d
fire1fly <danyseidev@gmail.com>
2024-01-14 08:02:00 +0800

    feat: add employee on main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,22 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Keyboard, Mousewheel } from 'swiper';
+import { MiniStatCard } from 'shared/ui/MiniStatCard/MiniStatCard';
+import { EmployeeCard } from 'entities/Employee';
 import PortfolioPage from '../PortfolioPage/PortfolioPage';
 import cls from './MainPage.module.scss';
+
+const employees = [
+  {
+    id: '1',
+    name: 'Daniel',
+    type: 'frontend',
+    position: 'Middle Frontend Developer',
+    stat: [
+      { type: 'case', value: 2 },
+      { type: 'experience', value: 21 },
+    ],
+  },
+];
 
 const MainPage = () => (
   <div className={cls.page}>
@@ -12,10 +27,11 @@ const MainPage = () => (
       keyboard
       className={cls.page_slider}
       mousewheel
-      onWheel={(e) => e.stopPropagation()}
     >
       <SwiperSlide>
-        <PortfolioPage />
+        <div className="px-20">
+          <EmployeeCard className="w-[325px]" item={employees[0]} />
+        </div>
       </SwiperSlide>
       <SwiperSlide>
         <PortfolioPage />
